c7203b4534c0d83993837e8f84e06087fc9b0fee
GuLuiz <gustaluizgonc@gmail.com>
2025-02-16 01:42:44 -0300

    Set the route to list as main

--- fontes/todolist-frontend/src/app/app.routes.ts
+++ fontes/todolist-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './admin/task/list/list.component';
+import { EditComponent } from './admin/task/edit/edit.component';
+import { AddComponent } from './admin/task/add/add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'list', pathMatch: 'full' },
+    {path:'list', component: ListComponent},
+    {path:'edit/:id', component: EditComponent},
+    {path:'add', component: AddComponent}
+
+];
